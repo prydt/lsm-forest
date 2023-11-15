dc23c21a2adbdcc6bf08498d26a6d17fd1f0b4c6
Jeongwoo CHoi <jeongwoo@utexas.edu>
2023-11-15 17:46:14 -0600

    bincode

--- src/lsm_forest.rs
+++ src/lsm_forest.rs
@@ -1,3 +1,5 @@
+use bincode::{Decode, Encode};
+
 pub mod lsm_forest {
     use std::{collections::BTreeMap, fs::File};
 
@@ -5,7 +7,29 @@ pub mod lsm_forest {
         file: File,
     }
 
+    #[derive(Encode, Decode, Ord, Debug)]
+    struct LogEntry<
+    K: Encode + Decode + Hash + Ord + 'static,
+    V: Encode + Decode + Hash + Ord + 'static,
+
+    >{
+    }
+
     struct LSMTree<K, V> {
         memtable: BTreeMap<K, V>,
+    }
+
+    impl<K, V> LSMTree<K, V> {
+        fn new() -> Self {
+            LSMTree {
+                memtable: BTreeMap::new(),
+            }
+        }
+    }
+
+    trait TableManager<K, V> {
+        fn get(&self, key: K) -> Option<V>;
+        fn put(&mut self, key: K, value: V);
+        fn delete(&mut self, key: K);
     }
 }
